604c151507f0fd0f9c22633d7a1421e6117b2454
Kapil Kumar <kapilgupta319mauryan@gmail.com>
2020-02-06 20:43:42 +0530

    Create next-permutation.cpp

---
+++ next-permutation.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        if(n==0 || n==1) return;
+        int index=-1;
+        for(int i=n-2;i>=0;--i){
+            if(nums[i]<nums[i+1]){
+                index = i;
+                break;
+            }
+        }
+        int large = index;
+        if(index<0){
+            reverse(nums.begin(),nums.end());
+        }
+        else{
+            for(int i=n-1;i>index;--i){
+                if(nums[index]<nums[i]){
+                    large = i;
+                    break;
+                }
+            }
+            swap(nums[index],nums[large]);
+            // cout << index << " "<<large;
+            reverse(nums.begin()+index+1, nums.end());
+        }
+    }
+};
