8ed2423b4cbe724aef9b98faaab0156f68438510
Kapil Kumar <kapilgupta319mauryan@gmail.com>
2020-02-10 16:03:11 +0530

    Update remove-nth-node-from-end-of-list.cpp

--- remove-nth-node-from-end-of-list.cpp
+++ remove-nth-node-from-end-of-list.cpp
@@ -24,6 +24,7 @@ Could you do this in one pass?
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+// Two pass algorithm
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
@@ -54,5 +55,36 @@ public:
             delete t;
             return head;
         }
+    }
+};
+
+// One pass algorithm
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode *dummy = new ListNode(0);
+        dummy->next = head;
+        
+        ListNode *slow = dummy, *fast = dummy;
+        for(int i=0;i<n+1;++i)
+            fast = fast->next;
+    
+        while(fast!=NULL){
+            slow = slow->next;
+            fast = fast->next;
+        }
+        
+        ListNode *temp = slow->next;
+        slow->next = slow->next->next;
+        delete temp;
+        return dummy->next;
     }
 };
