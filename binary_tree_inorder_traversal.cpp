beab6b7e66a0307a408fcda4f326e54054af181c
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-09-02 14:51:16 +0530

    Update binary_tree_inorder_traversal.cpp

--- binary_tree_inorder_traversal.cpp
+++ binary_tree_inorder_traversal.cpp
@@ -8,6 +8,9 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+// Using Stack
+
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
@@ -26,5 +29,58 @@ public:
             curr = curr->right;
         }
         return ans;
+    }
+};
+
+// Morris Traversal
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        TreeNode *pre = NULL;
+        
+        while(root!=NULL){
+            if(root->left==NULL){
+                ans.push_back(root->val);
+                root=root->right;
+            }
+            else{
+                pre = root->left;
+                while(pre->right!=NULL && pre->right!=root){
+                    pre = pre->right;
+                }
+                if(pre->right==NULL){
+                    pre->right = root;
+                    root = root->left;
+                }
+                else{
+                    pre->right=NULL;
+                    ans.push_back(root->val);
+                    root = root->right;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+// Using Recursion
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        inorder(root, ans);
+        return ans;
+    }
+    
+private:
+    void inorder(TreeNode* root, vector<int>& ans){
+        if(root!=NULL){
+            inorder(root->left, ans);
+            ans.push_back(root->val);
+            inorder(root->right, ans);
+        }
     }
 };
