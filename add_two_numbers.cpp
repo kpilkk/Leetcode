37cbc6d7e0bf5832ca2f3f24016fe25337ed9c69
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-09-03 18:44:30 +0530

    Update add_two_numbers.cpp

--- add_two_numbers.cpp
+++ add_two_numbers.cpp
@@ -10,26 +10,39 @@
 *Explanation: 342 + 465 = 807.
 **/
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        // sum = 0;
-        // int x = 0;
-        // int y = 0;
-        struct ListNode* res = new ListNode(0);
-        struct ListNode* ans = res;
+        ListNode* ans = new ListNode();
+        
+        ListNode* dummy = ans;
+        
         int carry = 0;
-        while(l1!=NULL || l2!=NULL || carry){
-            int x = (l1!=NULL)?l1->val:0;
-            int y = (l2!=NULL)?l2->val:0;
+        
+        while(l1 || l2 || carry){
+            int x = (l1!=NULL) ? l1->val : 0;
+            int y = (l2!=NULL) ? l2->val : 0;
             int sum = carry + x + y;
-            carry = sum/10;
-            res->next = new ListNode(sum%10);
-            res = res->next;
-            if(l1!=NULL) l1=l1->next;
-            if(l2!=NULL) l2=l2->next;
+            carry = sum / 10;
+            ans->next = new ListNode(sum % 10);
+            ans = ans->next;
+            if(l1) l1 = l1->next;
+            if(l2) l2 = l2->next;
         }
-        // if(carry>0) res->next = new ListNode(carry);
-        return ans->next;
+        
+        // if(carry > 0)
+        //     ans->next = new ListNode(carry);
+        
+        return dummy->next;
     }
 };
