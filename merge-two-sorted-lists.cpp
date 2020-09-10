eab7d244b58abb522c8117d54c20bb489c62b4c8
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-09-10 10:38:10 +0530

    Update merge-two-sorted-lists.cpp

--- merge-two-sorted-lists.cpp
+++ merge-two-sorted-lists.cpp
@@ -1,5 +1,31 @@
 // https://leetcode.com/problems/merge-two-sorted-lists/submissions/
 
+// Without using extra memory
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode dummy = ListNode(0);
+        ListNode* ans = &dummy;
+        
+        while(l1 && l2){
+            if(l1 -> val < l2 -> val){
+                ans -> next = l1;
+                l1 = l1 -> next;
+            }
+            else{
+                ans -> next = l2;
+                l2 = l2 -> next;
+            }
+            ans = ans -> next;
+        }
+        
+        ans -> next = l1 ? l1 : l2;
+        
+        return dummy.next;
+    }
+};
+
 // Using extra memory
 class Solution {
 public:
