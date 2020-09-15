a5ff6d24795c1c21d463d26c921a1e9c53d183fc
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-09-15 16:11:43 +0530

    Update median-of-two-sorted-arrays.cpp
    binary search method added

--- median-of-two-sorted-arrays.cpp
+++ median-of-two-sorted-arrays.cpp
@@ -42,3 +42,36 @@ public:
         return 0;
     }
 };
+
+
+// Binary search method
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size(), n = nums2.size();
+        if(m > n)
+            return findMedianSortedArrays(nums2, nums1);
+        
+        int l = 0, r = m;
+        
+        while(l <= r){
+            int i = (l + r) / 2, j = (m + n + 1) / 2 - i;
+            if(i && nums1[i - 1] > nums2[j])
+                // i is too big, must decrease it
+                r = i - 1;
+            else if(i < m && nums2[j - 1] > nums1[i])
+                // i is too small, must increase it
+                l = i + 1;
+            else{
+                // i is perfect
+                int lmax = !i ? nums2[j - 1] : (!j ? nums1[i - 1] : max(nums1[i - 1], nums2[j - 1]));
+                if((m + n) % 2)
+                    return lmax;
+                int rmin = i == m ? nums2[j] : (j == n ? nums1[i] : min(nums1[i], nums2[j]));
+                return 0.5 * (lmax + rmin);
+            }
+        }
+        return 0.0;
+    }
+};
