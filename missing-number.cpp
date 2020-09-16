e6e72f0d7148794d48cfa44b34f7d3ca87193a8c
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-09-16 12:29:35 +0530

    Update missing-number.cpp
    bit manipulation solution added

--- missing-number.cpp
+++ missing-number.cpp
@@ -18,3 +18,17 @@ public:
         return n;
     }
 };
+
+// Bit manipulation
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int missing = nums.size();
+        
+        for(int i = 0; i < nums.size(); ++i)
+            missing ^= i ^ nums[i];
+        
+        return missing;
+    }
+};
