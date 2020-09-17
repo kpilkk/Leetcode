9518da3aafc7262cb2499a1e4ce93dc31538debd
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-09-17 15:26:05 +0530

    Update kth-largest-element-in-an-array.cpp

--- kth-largest-element-in-an-array.cpp
+++ kth-largest-element-in-an-array.cpp
@@ -4,13 +4,11 @@ class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
         int n = nums.size();
-        if(n==0) return 0;
-        for(int i=0;i<k;++i){
-            for(int j=0;j<n-i-1;++j)
-                if(nums[j]>nums[j+1])
-                    swap(nums[j],nums[j+1]);
-        }
-        return nums[n-k];
+        for(int i = 0; i < k; ++i)
+            for(int j = 1; j < n - i; ++j)
+                if(nums[j - 1] > nums[j])
+                    swap(nums[j - 1], nums[j]);
+        return nums[n - k];
     }
 };
 
