aa11f1c8e68cc62bb298dbca3e2bd79c66052762
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-09-17 10:24:19 +0530

    Update linked-list-cycle-ii.cpp

--- linked-list-cycle-ii.cpp
+++ linked-list-cycle-ii.cpp
@@ -44,3 +44,37 @@ public:
         return slow;
     }
 };
+
+// 2nd solution
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(!head) return NULL;
+        bool cycle = false;
+        ListNode *slow = head, *fast = head;
+        while(fast && fast -> next){
+            slow = slow -> next;
+            fast = fast -> next -> next;
+            if(fast == slow){
+                cycle = true;
+                break;
+            }
+        }
+        if(!cycle) return NULL;
+        slow = head;
+        while(slow != fast){
+            slow = slow -> next;
+            fast = fast -> next;
+        }
+        return slow;
+    }
+};
