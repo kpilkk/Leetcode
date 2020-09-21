55f6585aad29b6868359268c6378d502955d25a4
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-09-21 23:39:03 +0530

    Update next-permutation.cpp

--- next-permutation.cpp
+++ next-permutation.cpp
@@ -27,3 +27,23 @@ public:
         }
     }
 };
+
+
+// Consice solution
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int i = nums.size() - 2;
+        while(i >= 0 && nums[i] >= nums[i + 1])
+            i--;
+        
+        if(i >= 0){
+            int j = nums.size() - 1;
+            while(j >= 0 && nums[j] <= nums[i])
+                j--;
+            swap(nums[j], nums[i]);
+        }
+        reverse(nums.begin() + i + 1, nums.end());
+    }
+};
