63b114a968872aa156bdd109c2e30d2b6d8e0593
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-09-26 11:03:33 +0530

    Update longest-palindromic-substring.cpp
    O(n2) solution added

--- longest-palindromic-substring.cpp
+++ longest-palindromic-substring.cpp
@@ -1,6 +1,6 @@
 // https://leetcode.com/problems/longest-palindromic-substring/
 
-// Brute Force : TLE
+// Brute Force : TLE : O(n3)
 
 class Solution {
 public:
@@ -26,5 +26,31 @@ private:
             if(s[i++] != s[j--])
                 return false;
         return true;
+    }
+};
+
+// O(n2) solution by expanding around centre
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        if(s.length() <= 1)
+            return s;
+        string longest = s.substr(0, 1);
+        for(int i = 0; i < s.length(); ++i){
+            string temp = expand(s , i , i);
+            if(temp.length() >= longest.length())
+                longest = temp;
+            temp = expand(s, i, i + 1);
+            if(temp.length() >= longest.length())
+                longest = temp;
+        }
+        return longest;
+    }
+private:
+    string expand(string s, int begin, int end){
+        while(begin >= 0 && end <= s.length() - 1 && s[begin] == s[end])
+            begin--, end++;
+        return s.substr(begin + 1, end - begin - 1);
     }
 };
