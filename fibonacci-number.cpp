fc4da6fa2643c91f40357e1647a57e067fef1512
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-09-27 07:39:11 +0530

    Create fibonacci-number.cpp
    recursion solution added

---
+++ fibonacci-number.cpp
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/fibonacci-number/
+
+// Approach 1 : Recursion
+class Solution {
+public:
+    int fib(int N) {
+        if(N == 0) return 0;
+        if(N == 1) return 1;
+        return fib(N - 1) + fib(N - 2);
+    }
+};
