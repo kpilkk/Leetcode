0220204be7fa7e8717a41e6c86a2f3820fcd3289
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-09-27 20:52:10 +0530

    Create ugly-number-ii.cpp

---
+++ ugly-number-ii.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/ugly-number-ii/
+
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        if(n == 1) return 1;
+        int i2 = 0, i3 = 0, i5 = 0;
+        vector<int> ugly(n);
+        ugly[0] = 1;
+        for(int i = 1; i < n; ++i){
+            ugly[i] = min(ugly[i2] * 2, min(ugly[i3] * 3, ugly[i5] * 5));
+
+            if(ugly[i] == ugly[i2] * 2)
+                ++i2;
+            if(ugly[i] == ugly[i3] * 3)
+                ++i3;
+            if(ugly[i] == ugly[i5] * 5)
+                ++i5;
+        }
+        return ugly[n - 1];
+    }
+};
