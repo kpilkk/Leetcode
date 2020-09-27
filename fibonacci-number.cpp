795a21adb13918195bc50663e147c84385a7ecbc
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-09-27 08:17:04 +0530

    Update fibonacci-number.cpp

--- fibonacci-number.cpp
+++ fibonacci-number.cpp
@@ -42,3 +42,23 @@ public:
         return cache[N];
     }
 };
+
+// Approach 4: Iterative Top-Down Approach
+class Solution {
+public:
+    int fib(int N) {
+        if(N <= 1) return N;
+        if(N == 2) return 1;
+        
+        int current  = 0;
+        int prev1 = 1;
+        int prev2 = 1;
+        
+        for(int i = 3; i <= N; ++i){
+            current = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = current;
+        }
+        return current;
+    }
+};
