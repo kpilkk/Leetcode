12b9fd4808fa15da7e70a9073fff2ffe6a550e5b
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-09-27 07:46:05 +0530

    Update fibonacci-number.cpp

--- fibonacci-number.cpp
+++ fibonacci-number.cpp
@@ -9,3 +9,17 @@ public:
         return fib(N - 1) + fib(N - 2);
     }
 };
+
+// Approach 2: Bottom-Up Approach using Memoization
+class Solution {
+public:
+    int fib(int N) {
+        int arr[N + 1];
+        arr[0] = 0;
+        if(N > 0)
+            arr[1] = 1;
+        for(int i = 2; i <= N; ++i)
+            arr[i] = arr[i - 1] + arr[i - 2];
+        return arr[N];
+    }
+};
