0b2db7feeac550c0461d704cc0a75ac584ddfcea
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-09-30 22:08:59 +0530

    Update longest-palindromic-substring.cpp
    updated Manachar's algorithm

--- longest-palindromic-substring.cpp
+++ longest-palindromic-substring.cpp
@@ -60,24 +60,47 @@ private:
 class Solution {
 public:
     string longestPalindrome(string s) {
-        string ss = "$";
-        for (char c : s) {
-            ss += "#";
-            ss += c;
-        }
-        ss += "#";
-        vector<int> Len(ss.size(), 0);
-        int pivot = 0, mx = 0;
-        for (int i = 1; i < ss.size(); ++i) {
-            if (i < mx) Len[i] = min(mx - i, Len[2 * pivot - i]);
-            else Len[i] = 1;
-            while (ss[i + Len[i]] == ss[i - Len[i]]) ++Len[i];
-            if (i + Len[i] >= mx) {
-                mx = i + Len[i];
-                pivot = i;
+        // Insert another 2 different special characters in the front and the end of string s to avoid bound checking.
+        string manacher_str = "@#";
+        for (int i = 0; i < s.size(); i++)
+            manacher_str = manacher_str + s[i] + "#";
+        manacher_str += "$";
+        
+        int len = manacher_str.size();
+        vector<int> RL(len, 0);
+        
+        int c = 0, r = 0;    // current center, right limit
+        int maxLen = 0, maxLenPos = 0;  //maxLen
+        
+        for (int i = 1; i < len - 1; i++) {
+            // find the corresponding letter in the palidrome subString
+            int iMirror = 2 * c - i;
+            if (r > i)
+                RL[i] = min(RL[iMirror], r - i);
+
+            // Let d = distance r - i`, so expanding around i` center will start from (i` - d) - 1
+            // with (i` + d) + 1 = (r + 1) and so on... 
+            // because the interval is already contained in the palindrome with center 
+            while (i - RL[i] >= 0 && i + RL[i] < len && manacher_str[i + RL[i] + 1] == manacher_str[i - RL[i] - 1])
+                RL[i]++;
+
+            if (RL[i] > maxLen) {
+                maxLen = RL[i];
+                maxLenPos = i;
+            }
+
+            // Update c, r in case if the palindrome centered at i expands past r
+            if (i + RL[i] > r) {
+                r = i + RL[i];
+                c = i;
             }
         }
-        int start = max_element(Len.begin(), Len.end()) - Len.begin();
-        return s.substr((start - Len[start]) / 2, Len[start] - 1);
+        
+        return s.substr((maxLenPos - 1 - maxLen) / 2, maxLen);
+        // s = "";
+        // for (int i = maxLenPos - maxLen + 1; i < maxLenPos + maxLen - 1; i++)
+        //     if (manacher_str[i] != '#')
+        //         s += manacher_str[i];
+        // return s;
     }
 };
