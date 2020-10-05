8fbe295fa60123157f24a8fc2daaefab9c890d82
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-10-05 21:57:34 +0530

    Update regular-expression-matching.cpp

--- regular-expression-matching.cpp
+++ regular-expression-matching.cpp
@@ -9,7 +9,7 @@ public:
         
         /* The most critical observation is that "x*" can either match empty string, 
         or at least one x. In the latter case, it is equivalent to "xx*" or "x*x"*/
-        if(p.length() >= 2 && p[1] == '*'){
+        if(p[1] == '*'){
             return (isMatch(s, p.substr(2)) || (first_match && isMatch(s.substr(1), p)));
         }
         else{
