9346a4d94fed351431a3ee28a60c15503d51bb15
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-10-06 11:13:30 +0530

    Update regular-expression-matching.cpp

--- regular-expression-matching.cpp
+++ regular-expression-matching.cpp
@@ -17,3 +17,30 @@ public:
         }
     }
 };
+
+// DP solution
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int m = s.size();
+        int n = p.size();
+        if(n >= 1 && p[0] == '*') return 0;
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
+        
+        dp[0][0] = true;
+        for(int i = 1; i <= m; ++i)
+            dp[i][0] = false;
+        for(int j = 1; j <= n; ++j)
+            dp[0][j] = j > 1 && p[j - 1] == '*' && dp[0][j - 2];
+        
+        for(int i = 1; i <= m; ++i){
+            for(int j = 1; j <= n; ++j){
+                if(p[j - 1] != '*')
+                    dp[i][j] = dp[i - 1][j - 1] && (s[i - 1] == p[j - 1] || p[j - 1] == '.');
+                else
+                    dp[i][j] = dp[i][j - 2] || ((s[i - 1] == p[j - 2] || p[j - 2] == '.') && dp[i - 1][j]);
+            }
+        }
+        return dp[m][n];
+    }
+};
