38c785eaaf523566b6d6d2efcab18841e53ca60f
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-10-07 16:08:36 +0530

    Create trapping-rain-water.cpp

---
+++ trapping-rain-water.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/trapping-rain-water/
+
+// Brute force method : O(n2)
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int ans = 0, n = height.size();
+        for(int i = 0; i < n; ++i){
+            int left_max = 0, right_max = 0;
+            for(int j = i; j >= 0; --j)
+                left_max = max(left_max, height[j]);
+            for(int j = i; j < n; ++j)
+                right_max = max(right_max, height[j]);
+            cout << left_max << " " << right_max << " ";
+            ans += min(left_max, right_max) - height[i];
+            cout << ans << endl;
+        }
+        return ans;
+    }
+};
