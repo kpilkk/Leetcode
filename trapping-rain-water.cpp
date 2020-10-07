0b84659d57fe6c8254a97d8484cc16ba8e0c0ba0
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-10-07 20:10:07 +0530

    Update trapping-rain-water.cpp

--- trapping-rain-water.cpp
+++ trapping-rain-water.cpp
@@ -36,3 +36,20 @@ public:
         return ans;
     }
 };
+
+// Two pointer
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int left = 0, right = height.size() - 1;
+        int ans = 0;
+        int left_max = 0, right_max = 0;
+        while(left < right){
+            if(height[left] < height[right])
+                height[left] >= left_max ? (left_max = height[left++]) : ans += (left_max - height[left++]);
+            else
+                height[right] >= right_max ? (right_max = height[right--]) : ans += (right_max - height[right--]);
+        }
+        return ans;
+    }
+};
