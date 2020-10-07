6eec91a496df4368eb54c0fdb1cba52f76949c50
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-10-07 16:48:02 +0530

    Update trapping-rain-water.cpp

--- trapping-rain-water.cpp
+++ trapping-rain-water.cpp
@@ -16,3 +16,23 @@ public:
         return ans;
     }
 };
+
+// Dynamic Programming
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int ans = 0;
+        int n = height.size();
+        if(n == 0) return 0;
+        vector<int> left_max(n), right_max(n);
+        left_max[0] = height[0];
+        for(int i = 1; i < n; ++i)
+            left_max[i] = max(height[i], left_max[i - 1]);
+        right_max[n - 1] = height[n - 1];
+        for(int i = n - 2; i >= 0; --i)
+            right_max[i] = max(height[i], right_max[i + 1]);
+        for(int i = 0; i < n; ++i)
+            ans += min(left_max[i], right_max[i]) - height[i];
+        return ans;
+    }
+};
