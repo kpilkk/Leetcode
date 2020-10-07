2b80cee9fae4eec0a6132165132d63d35a5a83bd
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-10-07 21:04:12 +0530

    Update wildcard-matching.cpp

--- wildcard-matching.cpp
+++ wildcard-matching.cpp
@@ -1,5 +1,48 @@
 // https://leetcode.com/problems/wildcard-matching/
 
+// Brute Force : TLE
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        if(p.empty())
+            return s.empty();
+        else if(s.empty())
+            return p[0] == '*' && isMatch(s, p.substr(1));
+        else if(p[0] == '*')
+            return isMatch(s, p.substr(1)) || isMatch(s.substr(1), p);
+        else if(p[0] == '?' || p[0] == s[0])
+            return isMatch(s.substr(1), p.substr(1));
+        else
+            return false;
+    }
+};
+
+// DP solution
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int m = s.size(), n = p.size();
+        vector<vector<bool>> dp(m + 1, vector<bool> (n + 1, false));
+            
+        for(int i = 0; i <= m; ++i){
+            for(int j = 0; j <= n; ++j){
+                if(i == 0 && j == 0)
+                    dp[i][j] = true;
+                else if(i == 0)
+                    dp[i][j] = p[j - 1] == '*' && dp[i][j - 1];
+                else if(j == 0)
+                    dp[i][j] = false;
+                else if(p[j - 1] == '*')
+                    dp[i][j] = dp[i][j - 1] || dp[i - 1][j]; 
+                else
+                    dp[i][j] = dp[i - 1][j - 1] && (p[j - 1] == '?' || p[j - 1] == s[i - 1]);
+            }
+        }
+        return dp[m][n];
+    }
+};
+
+// O(n) solution
 class Solution {
 public:
     bool isMatch(string s, string p) {
