ac95fa79a893ec60b5ae522ae0b08a18cbfad542
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-10-11 10:52:07 +0530

    Update longest-common-prefix.cpp

--- longest-common-prefix.cpp
+++ longest-common-prefix.cpp
@@ -31,3 +31,24 @@ public:
         return strs[0];
     }
 };
+
+// By sorting and comparing first and last string
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.size() == 0) return "";
+        sort(strs.begin(), strs.end());
+        int n = strs.size() - 1;
+        int m = min(strs[0].length(), strs[n].length());
+        
+        int i = 0, j = 0;
+        while(i < m){
+            if(strs[0][i] == strs[n][i])
+                j++;
+            else
+                break;
+            i++;
+        }
+        return strs[0].substr(0, j);
+    }
+};
