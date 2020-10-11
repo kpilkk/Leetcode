1cb2852e3d1ac8bfdcf5ace359c2ff8f9b895256
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-10-11 11:10:40 +0530

    Update longest-common-prefix.cpp
    divide and conquer solution added

--- longest-common-prefix.cpp
+++ longest-common-prefix.cpp
@@ -52,3 +52,31 @@ public:
         return strs[0].substr(0, j);
     }
 };
+
+// Divide and Conquer
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.size() == 0) return "";
+        return longestCommonPrefix(strs, 0, strs.size() - 1);
+    }
+private:
+    string longestCommonPrefix(vector<string> strs, int l, int r){
+        if(l == r)
+            return strs[l];
+        else{
+            int mid = ( l + r ) / 2;
+            string lcpLeft = longestCommonPrefix(strs, l, mid);
+            string lcpRight = longestCommonPrefix(strs, mid + 1, r);
+            return commonPrefix(lcpLeft, lcpRight);
+        }
+    }
+    
+    string commonPrefix(string left, string right){
+        int mini = min(left.length(), right.length());
+        for(int i = 0; i < mini; ++i)
+            if(left[i] != right[i])
+                return left.substr(0, i);
+        return left.substr(0, mini);
+    }
+};
