d5e0127aacc307987bbdc968c79c439283f8606a
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-10-24 14:40:13 +0530

    Update swap-nodes-in-pairs.cpp

--- swap-nodes-in-pairs.cpp
+++ swap-nodes-in-pairs.cpp
@@ -14,3 +14,26 @@ public:
         return nxt;
     }
 };
+
+// Iterative solution using constant space
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(!head)
+            return head;
+        ListNode dummy(0);
+        ListNode* pre = &dummy;
+        
+        pre -> next = head;
+        
+        while(head && head -> next){
+            ListNode* temp = head -> next;
+            head -> next = temp -> next;
+            temp -> next = head;
+            pre -> next = temp;
+            pre = head;
+            head = head -> next;
+        }
+        return dummy.next;
+    }
+};
