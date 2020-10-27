bd10650596c804fa8386dba3fb2950d3c0c6c835
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-10-27 16:18:59 +0530

    Update implement-strstr.cpp

--- implement-strstr.cpp
+++ implement-strstr.cpp
@@ -1,5 +1,35 @@
 // https://leetcode.com/problems/implement-strstr/
 
+// fastest
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if(needle.empty())
+            return 0;
+        
+        string kmp = needle + "#" + haystack;
+        
+        int m = kmp.size(), n = needle.size();
+        
+        vector<int> lps(m , 0);
+        
+        for(int i = 1, len = 0; i < m;){
+            if(kmp[i] == kmp[len]){
+                lps[i] = ++len;
+                if(len == n)
+                    return i - 2 * n;
+                ++i;
+            }
+            else if(len == 0)
+                lps[i++] = 0;
+            else
+                len = lps[len - 1];
+        }
+        return -1;
+    }
+};
+
+// Brute forcc
 class Solution {
 public:
     int strStr(string haystack, string needle) {
