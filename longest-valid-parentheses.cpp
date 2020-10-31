5b4aefe684bc350b5448d2bf1bb16966ade74a47
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-10-31 18:38:41 +0530

    Update longest-valid-parentheses.cpp

--- longest-valid-parentheses.cpp
+++ longest-valid-parentheses.cpp
@@ -70,3 +70,37 @@ public:
         return ans;
     }
 };
+
+// Without extra space
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int left = 0, right = 0, ans = 0, n = s.length();
+        
+        for(int i = 0; i < n; ++i){
+            if(s[i] == '(')
+                ++left;
+            else
+                ++right;
+            if(left == right)
+                ans = max(ans , 2 * right);
+            else if(right > left)
+                left = right = 0;
+        }
+        
+        left = right = 0;
+        
+        for(int i = n - 1; i >= 0; --i){
+            if(s[i] == '(')
+                ++left;
+            else
+                ++right;
+            if(left == right)
+                ans = max(ans, 2 * right);
+            else if(left > right)
+                left = right = 0;
+        }
+        
+        return ans;
+    }
+};
