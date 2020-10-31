800e4894a198f259e07d8a37714e03783fc3e766
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-10-31 16:14:02 +0530

    Update longest-valid-parentheses.cpp

--- longest-valid-parentheses.cpp
+++ longest-valid-parentheses.cpp
@@ -26,3 +26,24 @@ public:
         return temp.empty();
     }
 };
+
+// DP solution
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int n = s.length();
+        int ans = 0;
+        
+        vector<int> dp(n , 0);
+        for(int i = 1; i < n; ++i){
+            if(s[i] == ')'){
+                if(s[i - 1] == '(')
+                    dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+                else if(i - dp[i - 1] > 0 && s[i - dp[i - 1] - 1] == '(')
+                    dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
+            }
+            ans = max(dp[i], ans);
+        }
+        return ans;
+    }
+};
