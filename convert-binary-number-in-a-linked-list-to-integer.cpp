843328d4cc0a2a299740c11e036445386a220688
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-11-01 14:44:26 +0530

    Update convert-binary-number-in-a-linked-list-to-integer.cpp

--- convert-binary-number-in-a-linked-list-to-integer.cpp
+++ convert-binary-number-in-a-linked-list-to-integer.cpp
@@ -26,11 +26,11 @@ public:
 class Solution {
 public:
     int getDecimalValue(ListNode* head) {
-        int ans=0;
-        while(head!=NULL){
-            ans<<=1;
-            ans+=head->val ;
-            head=head->next;
+        int ans = 0;
+        while(head != NULL){
+            ans <<= 1;
+            ans += head -> val ;
+            head = head -> next;
         }
         return ans;
     }
