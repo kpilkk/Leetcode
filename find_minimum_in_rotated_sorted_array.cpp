2df04c553176522f248312b0975dc9204ec1303b
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-11-02 16:42:20 +0530

    Update find_minimum_in_rotated_sorted_array.cpp

--- find_minimum_in_rotated_sorted_array.cpp
+++ find_minimum_in_rotated_sorted_array.cpp
@@ -1,19 +1,47 @@
 // https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
 
+// 1st solution
 class Solution {
 public:
     int findMin(vector<int>& nums) {
         int n = nums.size();
-        if(n==0) return -1;
+        if(n == 0) 
+            return -1;
         
         int low = 0, high = n-1;
-        while(nums[low]>nums[high]){
-            int mid = low+(high-low)/2;
-            if(nums[mid]>=nums[low])
-                low = mid+1;
-            else if(nums[mid]<nums[high])
+        while(nums[low] > nums[high]){
+            int mid = low + (high - low) / 2;
+            if(nums[mid] >= nums[low])
+                low = mid + 1;
+            else if(nums[mid] < nums[high])
                 high = mid;
         }
         return nums[low];
+    }
+};
+
+// 2nd solution
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int n = nums.size();
+        int l = 0, r = n - 1;
+        if(n == 1)
+            return nums[0];
+        if(nums[r] > nums[l])
+            return nums[l];
+        
+        while(l <= r){
+            int mid = l + (r - l) / 2;
+            if(nums[mid] > nums[mid + 1])
+                return nums[mid + 1];
+            if(nums[mid] < nums[mid - 1])
+                return nums[mid];
+            if(nums[mid] > nums[l])
+                l = mid + 1;
+            else
+                r = mid;
+        }
+        return -1;
     }
 };
