fabcde99c36e147365806c354e734c02c4bc154a
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-11-03 10:40:39 +0530

    Update find_minimum_in_rotated_sorted_array_ii.cpp

--- find_minimum_in_rotated_sorted_array_ii.cpp
+++ find_minimum_in_rotated_sorted_array_ii.cpp
@@ -42,3 +42,25 @@ public:
         return nums[l];
     }
 };
+
+// 3rd solution
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int left=0;
+        int right=nums.size()-1;
+        while(left<right && nums[left]>=nums[right]){
+            int mid=left+(right-left)/2;
+            if(nums[mid]==nums[left]){
+                ++left;
+            }
+            else if(nums[mid]<nums[left]){
+                right=mid;
+            }
+            else{
+                left=mid+1;
+            }
+        }
+        return nums[left];
+    }
+};
