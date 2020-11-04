d6821377de15fffe958358d226305d2e6f599d92
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-11-04 16:18:32 +0530

    Update sum-of-root-to-leaf-binary-numbers.cpp

--- sum-of-root-to-leaf-binary-numbers.cpp
+++ sum-of-root-to-leaf-binary-numbers.cpp
@@ -19,3 +19,31 @@ public:
         dfs(root -> right, val, ans);
     }
 };
+
+// BFS iterative solution
+class Solution {
+public:
+    int sumRootToLeaf(TreeNode* root) {
+        if(!root)
+            return 0;
+        
+        queue<pair<TreeNode*, int>> bfs;
+        bfs.push(make_pair(root, root -> val));
+        
+        int ans = 0;
+        pair<TreeNode*, int> curr;
+        while(!bfs.empty()){
+            curr = bfs.front();
+            bfs.pop();
+            
+            if(curr.first -> left == curr.first -> right)
+                ans += curr.second;
+            
+            if(curr.first -> left)
+                bfs.push(make_pair(curr.first -> left, (curr.second << 1 | curr.first -> left -> val)));
+            if(curr.first -> right)
+                bfs.push(make_pair(curr.first -> right, (curr.second << 1 | curr.first -> right -> val)));
+        }
+        return ans;
+    }
+};
