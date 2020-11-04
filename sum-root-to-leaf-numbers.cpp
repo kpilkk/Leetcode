8bbf4688c1bd785d9fd48b40ed4a4eedac42a535
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-11-04 19:38:32 +0530

    Update sum-root-to-leaf-numbers.cpp

--- sum-root-to-leaf-numbers.cpp
+++ sum-root-to-leaf-numbers.cpp
@@ -31,3 +31,31 @@ public:
         dfs(root -> right, val, ans);
     }
 };
+
+// BFS iterative : beats 100% runtime
+class Solution {
+public:
+    int sumNumbers(TreeNode* root) {
+        if(!root)
+            return 0;
+        int ans = 0;
+        
+        queue<pair<TreeNode*, int>> bfs;
+        bfs.push(make_pair(root, root -> val));
+        pair<TreeNode*, int> curr;
+        
+        while(!bfs.empty()){
+            curr = bfs.front();
+            bfs.pop();
+            
+            if(curr.first -> left == curr.first -> right)
+                ans += curr.second;
+            
+            if(curr.first -> left)
+                bfs.push(make_pair(curr.first -> left, (curr.second * 10 + curr.first -> left -> val)));
+            if(curr.first -> right)
+                bfs.push(make_pair(curr.first -> right, (curr.second * 10 + curr.first -> right -> val)));
+        }
+        return ans;
+    }
+};
