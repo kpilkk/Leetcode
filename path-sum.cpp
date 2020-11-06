c497355a655a640c3611d203aa8d65a1a41894c6
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-11-06 14:12:02 +0530

    Create path-sum.cpp

---
+++ path-sum.cpp
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/path-sum/
+
+// Recursive solution
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        if(!root)
+            return false;
+        
+        if(root -> left == nullptr && root -> right == nullptr)
+            return sum == root -> val;
+                       
+        return hasPathSum(root -> left, sum - (root -> val)) || hasPathSum(root -> right, sum - (root -> val));
+    }
+};
