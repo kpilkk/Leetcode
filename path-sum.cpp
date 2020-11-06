1b2854dd7062f25ee3cfbdb2225d25923b6a2a46
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-11-06 14:32:22 +0530

    Update path-sum.cpp

--- path-sum.cpp
+++ path-sum.cpp
@@ -13,3 +13,27 @@ public:
         return hasPathSum(root -> left, sum - (root -> val)) || hasPathSum(root -> right, sum - (root -> val));
     }
 };
+
+// BFS iterative solution
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        if(!root)
+            return false;
+        
+        queue<pair<TreeNode*, int>> bfs;
+        bfs.push({root, 0});
+        while(!bfs.empty()){
+            auto temp = bfs.front();
+            bfs.pop();
+            int s = temp.first -> val + temp.second;
+            if((temp.first -> left == temp.first -> right) && s == sum)
+                return true;
+            if(temp.first -> left)
+                bfs.push({temp.first -> left, s});
+            if(temp.first -> right)
+                bfs.push({temp.first -> right, s});
+        }
+        return false;
+    }
+};
