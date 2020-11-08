340fdd1bce1e5894e780a530266d583cc9b12db0
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-11-08 20:49:17 +0530

    Update binary-tree-postorder-traversal.cpp

--- binary-tree-postorder-traversal.cpp
+++ binary-tree-postorder-traversal.cpp
@@ -17,3 +17,28 @@ public:
         ans.emplace_back(root -> val);
     }
 };
+
+// Iterative solution using stack
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        stack<TreeNode*> stack;
+        TreeNode* last = nullptr;
+        while(root || !stack.empty()){
+            while(root){
+                stack.emplace(root);
+                root = root -> left;
+            }
+            TreeNode* temp = stack.top();
+            if(temp -> right && last != temp -> right)
+                root = temp -> right;
+            else{
+                ans.emplace_back(temp -> val);
+                last = temp;
+                stack.pop();
+            }
+        }
+        return ans;
+    }
+};
