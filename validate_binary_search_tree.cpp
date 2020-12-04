626cbec6da1c95b4ce5a68ba786482a972ac669c
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-12-04 20:39:06 +0530

    Update validate_binary_search_tree.cpp

--- validate_binary_search_tree.cpp
+++ validate_binary_search_tree.cpp
@@ -1,12 +1,26 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+// https://leetcode.com/problems/validate-binary-search-tree/
+
+// recursive solution
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        return isValid(root, NULL, NULL);
+    }
+    
+    bool isValid(TreeNode* root, int* lower, int* upper){
+        if(!root)
+            return true;
+
+        if(upper && root -> val >= *upper)
+            return false;
+        if(lower && root -> val <= *lower)
+            return false;
+        
+        return isValid(root -> left, lower, &(root -> val)) && isValid(root -> right, &(root -> val), upper);
+    }
+};
+
+// stack based solution - iterative
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
