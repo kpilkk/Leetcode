6c9a13201136496c6fd09206e6a6cf1861098581
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-12-06 19:49:53 +0530

    Update search-in-a-binary-search-tree.cpp

--- search-in-a-binary-search-tree.cpp
+++ search-in-a-binary-search-tree.cpp
@@ -1,5 +1,20 @@
 // https://leetcode.com/problems/search-in-a-binary-search-tree/
 
+// Recursive solution
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(!root)
+            return root;
+        if(val < root -> val)
+            return searchBST(root -> left, val);
+        else if(val > root -> val)
+            return searchBST(root -> right, val);
+        else
+            return root;
+    }
+};
+
 // Iterative solution
 class Solution {
 public:
