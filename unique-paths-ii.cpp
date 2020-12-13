1f00d3ea940959b6d6c4f492a8d512a9f928edde
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2020-12-13 20:00:13 +0530

    Update unique-paths-ii.cpp

--- unique-paths-ii.cpp
+++ unique-paths-ii.cpp
@@ -31,3 +31,40 @@ public:
         return dp[m - 1][n - 1];
     }
 };
+
+// Modification of above but using no extra memory
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+
+        if(obstacleGrid[0][0] == 1)
+            return 0;
+        
+        obstacleGrid[0][0] = 1;
+            
+        for(int i = 1; i < n; ++i)
+            if(obstacleGrid[0][i] == 1)
+                while(i < n)
+                    obstacleGrid[0][i++] = 0;
+            else
+                obstacleGrid[0][i] = 1;
+        
+        for(int i = 1; i < m; ++i)
+            if(obstacleGrid[i][0] == 1)
+                while(i < m)
+                    obstacleGrid[i++][0] = 0;
+            else
+                obstacleGrid[i][0] = 1;
+        
+        for(int i = 1; i < m; ++i)
+            for(int j = 1; j < n; ++j)
+                if(obstacleGrid[i][j] == 1)
+                    obstacleGrid[i][j] = 0;
+                else
+                    obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1];
+        
+        return obstacleGrid[m - 1][n - 1];
+    }
+};
