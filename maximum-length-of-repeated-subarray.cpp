9f68987e704450fa8845aa9ed98d366e8b8151f8
Kapil Kumar <kapilgupta319mauryan@gmail.com>
2021-07-10 13:20:13 +0530

    added max length of repeated subarray

---
+++ maximum-length-of-repeated-subarray.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/maximum-length-of-repeated-subarray/
+
+// Brute force time limit exceeded
+class Solution {
+public:
+    int findLength(vector<int>& nums1, vector<int>& nums2) {
+        int ans = 0;
+        int m = nums1.size();
+        int n = nums2.size();
+        for(int i = 0; i < m; ++i)
+            for(int j = 0; j < n; ++j){
+                int k = 0;
+                while(i + k < m && j + k < n && nums1[i + k] == nums2[j + k])
+                    k += 1;
+                ans = max(ans, k);
+            }
+        return ans;
+    }
+};
