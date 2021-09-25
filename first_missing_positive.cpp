6abed14708cbf362545d203f02928dac11dfd5fc
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2021-09-25 13:47:13 +0530

    more approaches of first missing integer added

--- first_missing_positive.cpp
+++ first_missing_positive.cpp
@@ -15,3 +15,56 @@ public:
         return n+1;
     }
 };
+
+
+// 2nd solution from GFG
+
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        bool one = false;
+        int n = nums.size();
+        
+        for(int i = 0; i < nums.size(); ++i){
+            if(nums[i] == 1){
+                one = true;
+                break;
+            }
+        }
+        
+        if(!one)
+            return 1;
+        
+        for(int i = 0; i < nums.size(); ++i){
+            if(nums[i] <= 0 || nums[i] > n)
+                nums[i] = 1;
+        }
+        
+        for(int i = 0; i < nums.size(); ++i){
+            nums[(nums[i] - 1) % n] += n;
+        }
+        
+        for(int i = 0; i < n; ++i)
+            if(nums[i] <= n)
+                return i + 1;
+        
+        return n + 1;
+    }
+};
+
+// #rd approach
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        for(int i = 0; i < nums.size(); ++i){
+            while(nums[i] > 0 && nums[i] <= nums.size() && nums[i] != nums[nums[i] - 1])
+                swap(nums[i], nums[nums[i] - 1]);
+        }
+        
+        for(int i = 0; i < nums.size(); ++i)
+            if(nums[i] != i + 1)
+                return i + 1;
+        
+        return nums.size() + 1;
+    }
+};
