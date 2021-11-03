0cc5c1cb5b0a10f0fe210450646b274d10dac882
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2021-11-03 19:38:34 +0530

    move zeroes updated

--- move_zeroes.cpp
+++ move_zeroes.cpp
@@ -15,3 +15,19 @@ public:
             nums[i] = 0;
     }
 };
+
+// Easy two pointer solution
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i = 0;
+        int j = 0;
+        
+        while(i < nums.size()){
+            if(nums[i] != 0){
+                swap(nums[i], nums[j++]);
+            }
+            ++i;
+        }
+    }
+};
